87cc0f8dd2d441d3f3cc88841fcfe5d1f1daf8ba
«d16» <s.ka@yandex.ru>
2023-06-20 13:37:02 +0300

    Example 005 if else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.Write("Введите ваше имя:  ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "pups")
+ {
+
+    Console.Write($"Ура это же  ",username);
+ }
+
+else
+
+    Console.Write("Привет:  ");
+    Console.WriteLine(username);
