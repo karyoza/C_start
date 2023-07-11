5adb615f16a133ed8fc52452ff1fec077f2e61ad
«d16» <s.ka@yandex.ru>
2023-07-11 21:39:39 +0300

    Example 012 Methods finish

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -64,4 +64,105 @@ result = result + text;
 return result;
 }
 string res = Metod4(10, "asdf");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+
+//for (int i = 2; i < 10; i++)
+{
+//for (int j = 2; j < 10; j++)
+//{
+
+  //  Console.WriteLine($"{i}*{j} = {i*j}");
+}
+//Console.WriteLine();
+//}
+
+
+//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+
+
+// string Replace(string text,char oldValue, char newValue);
+// {
+// string result = string.Empty;
+
+// int length = text.Length;
+// for (int i = 0; i < length;, i++);
+//     {
+//      if (text{i} == newValue) result = result + $"{ newValue}";
+
+//     else result = result+ $"{text[i]}";
+//     }
+
+
+// return result;
+
+// }
+
+// string newText = Replace(text, " ", "|");
+// Console.WriteLine(newText);
+
+
+
+
+// рабочий код ниже !
+
+// string text = @"— Я думаю, — сказал князь, улыбаясь, — что, 
+// ежели бы вас послали вместо нашего милого Винценгероде, 
+// вы бы взяли приступом согласие прусского короля.
+// Вы так красноречивы. Вы дадите мне чаю?" ;
+
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+// string result = String.Empty;
+// int length = text.Length ;
+// for (int i = 0; i < length; i++)
+// {
+// if(text[i] == oldValue) result = result + $"{newValue}";
+// else result = result + $"{text[i]}";
+// }
+// return result;
+// }
+// string newText = Replace(text, ' ', '|') ;
+// Console.WriteLine(newText);
+
+
+
+int [] arr = {1,4,7,5,9,6,1,7};
+
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i ++)
+     Console.Write($" {array[i]} ");
+}
+Console.WriteLine();
+
+
+void SelectionSort(int [] array)
+{
+
+    for (int i = 0; i < array.Length - 1 ; i ++)
+    {
+       int minPosition = i;
+       for (int j = i + 1; j < array.Length ; j ++)
+       {
+        if(array[j] < array[minPosition]) minPosition = j;
+       }
+
+
+    int temporary = array[i];
+     array[i] = array[minPosition];
+     array[minPosition] = temporary;
+    }
+}
+
+
+
+PrintArray(arr);
+SelectionSort(arr);
+Console.WriteLine("");
+PrintArray(arr);
